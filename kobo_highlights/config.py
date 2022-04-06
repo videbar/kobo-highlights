b884458e531d696f7f3d30f434741cb5c0af6ada
Pedro Videgain Barranco <p.videgain.barranco@fz-juelich.de>
2022-04-06 18:09:34 +0200

    Created config.py to hold the program configuration

---
+++ kobo_highlights/config.py
@@ -0,0 +1,94 @@
+from __future__ import annotations
+from .console import console
+from pathlib import Path
+from pydantic import BaseModel, Extra, ValidationError, validate_arguments
+from rich.prompt import Prompt
+from rich.panel import Panel
+from rich.table import Table
+import toml
+
+
+class Config(BaseModel, extra=Extra.forbid):
+    target_dir: Path
+    ereader_dir: Path
+
+    @classmethod
+    def from_file(cls, config_path: Path) -> Config:
+        try:
+            return cls.parse_obj(toml.load(config_path))
+
+        except FileNotFoundError:
+            raise ConfigError(f"No config file was found in: {config_path}")
+
+        except (toml.TomlDecodeError, TypeError):
+            raise ConfigError(f"The config file {config_path} is not a valid toml file")
+
+        except ValidationError:
+            raise ConfigError(
+                f"The config file {config_path} is not a valid configuration file"
+            )
+
+    @classmethod
+    def create_interactively(cls) -> Config:
+        while True:
+            while True:
+                ereader_input: str = Prompt.ask(
+                    "Please enter the absolute path where your [bold]ereader[/] is"
+                    " mounted",
+                    console=console,
+                )
+
+                target_input: str = Prompt.ask(
+                    "Please enter the absolute path to the [bold]target file[/] where"
+                    " the highlights will be exported",
+                    console=console,
+                )
+
+                try:
+                    ereader_dir, target_dir = Path(ereader_input), Path(target_input)
+
+                    if ereader_dir.is_absolute() and target_dir.is_absolute():
+                        config = cls(
+                            target_dir=Path(target_input),
+                            ereader_dir=Path(ereader_dir),
+                        )
+
+                        return config
+
+                    else:
+                        console.print(
+                            "[prompt.invalid]The paths entered are not absolute"
+                        )
+
+                except TypeError:  # User input cannot be converted into path
+                    console.print("[prompt.invalid]The inputs entered are not paths")
+
+    def save_file(self, config_path: Path) -> Config:
+        with open(config_path, "w") as config_file:
+            toml.dump(self.dict(), config_file)
+
+        return self
+
+    def __rich__(self) -> Table:
+        """Required for rendering the config objects using Rich:
+        https://rich.readthedocs.io/en/stable/protocol.html#console-customization
+        """
+        config_table = Table(box=None, show_header=False)
+
+        config_table.add_row(
+            "target_dir:",
+            f"[cyan]{str(self.target_dir)}",
+            "[bright_black]# Directory where your highlights will be exported ",
+        )
+
+        config_table.add_row(
+            "ereader_dir:",
+            f"[cyan]{str(self.ereader_dir)}",
+            "[bright_black]# Directory where your ereader is mounted ",
+        )
+        return config_table
+
+
+class ConfigError(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
--- kobo_highlights/main.py
+++ kobo_highlights/main.py
@@ -1,5 +1,30 @@
 import typer
+from pathlib import Path
 
-a = []
+app = typer.Typer()
+APP_NAME = "kobo_highlights"
 
-print(a)
+@app.command()
+def add(filename: str):
+    typer.echo("test 2")
+
+@app.command()
+def add_all():
+    typer.echo("test 3")
+
+
+def read_config(app_name) -> dict:
+    """_summary_
+
+    Args:
+        app_name (_type_): _description_
+
+    Returns:
+        dict: _description_
+    """
+
+    config_dir: Path = Path(typer.get_app_dir(app_name)).resolve()
+    with open(config_dir/"config.toml", "r") as config_file:
+        config: dict = toml.load(config_file)
+
+    return config
