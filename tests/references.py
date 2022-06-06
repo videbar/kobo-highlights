6b544260d9e0e42388eca498218b7f1b6f54c2e8
Pedro Videgain Barranco <pedrovb04@protonmail.com>
2022-06-06 12:50:10 +0200

    Added tests for funtions.py

---
+++ tests/references.py
@@ -0,0 +1,144 @@
+"""This simple module contains references used in the tests:
+
+* BOOKMARKS_TO_ADD: A list of bookmarks that, when imported to markdown, would result
+in the REFERENCE_MARKDOWN document.
+
+* REFERENCE_MARKDOWN: A list of reference markdown text that results from importing the
+bookmarks in BOOKMARKS_TO_ADD. The list contains two markdown documents, one corresponds
+to the first bookmark in BOOKMARKS_TO_ADD being imported and the second one corresponds
+to the second bookmark in BOOKMARKS_TO_ADD being imported to a file that already exists.
+
+* BOOKMARKS_QUERIED_FROM_MD: A list of how the two bookmarks in REFERENCE_MARKDOWN
+should look when queried.
+
+* EXPECTED_BOOKMARKS_SQLITE: A list of the bookmarks contained in the sqlite file
+that ships with the tests.
+"""
+
+BOOKMARKS_TO_ADD: list[dict] = [
+    {
+        "id": "11111111-111111111-11111111111111111",
+        "text": "I am a bookmark with multiple lines.\nI continue here",
+        "annotation": None,
+        "author": "Author name with a -in the middle (no spaces)",
+        "title": "Book title with a - in the middle",
+    },
+    {
+        "id": "11111111-111111111-11111111111111111",
+        "text": "I am a bookmark",
+        "annotation": (
+            "I am an annotation with multiple lines\n"
+            "and multiple paragraphs:\n"
+            "\n"
+            "I continue here"
+        ),
+        "author": "Author name with a -in the middle (no spaces)",
+        "title": "Book title with a - in the middle",
+    },
+]
+
+REFERENCE_MARKDOWN = [
+    "\n> I am a bookmark with multiple lines.\n> I continue here",
+    (
+        "\n"
+        "\n"
+        "***\n"
+        "\n"
+        "> I am a bookmark\n"
+        "\n"
+        "I am an annotation with multiple lines\n"
+        "and multiple paragraphs:\n"
+        "\n"
+        "I continue here"
+    ),
+]
+
+BOOKMARKS_QUERIED_FROM_MD: list[dict] = [
+    {
+        "text": "I am a bookmark with multiple lines.\nI continue here",
+        "author": "Author name with a -in the middle (no spaces)",
+        "title": "Book title with a - in the middle",
+    },
+    {
+        "text": "I am a bookmark",
+        "author": "Author name with a -in the middle (no spaces)",
+        "title": "Book title with a - in the middle",
+    },
+]
+
+# This list contains the bookmarks that are inside the reference sqlite file that is
+# shipped with the tests and is used to test the sqlite query function.
+EXPECTED_BOOKMARKS_SQLITE: list[dict] = [
+    {
+        "id": "709644b4-03cb-448c-8b55-06c63cc26308",
+        "text": "normales",
+        "annotation": None,
+        "author": "Le Guin, Ursula K",
+        "title": "La mano izquierda de la oscuridad",
+    },
+    {
+        "id": "614009e0-8f62-4d25-9cef-6b3bc7224b6f",
+        "text": "Tienes que llegar a creer que es inútil para poder practicarla.\n",
+        "annotation": None,
+        "author": "Le Guin, Ursula K",
+        "title": "La mano izquierda de la oscuridad",
+    },
+    {
+        "id": "b2c73ce6-4144-40d4-b924-9683db85c7c7",
+        "text": (
+            "Statements are instructions that perform some action and do not"
+            " return a value. Expressions evaluate to a resulting value"
+        ),
+        "annotation": None,
+        "author": "Klabnik, Steve & Nichols, Carol",
+        "title": "The Rust Programming Language",
+    },
+    {
+        "id": "464d0e9d-61cb-4a99-9399-34c586120124",
+        "text": (
+            "Rust’s enums are most similar to algebraic data types in functional"
+            " languages, such as F#, OCaml, and Haskell."
+        ),
+        "annotation": None,
+        "author": "Klabnik, Steve & Nichols, Carol",
+        "title": "The Rust Programming Language",
+    },
+    {
+        "id": "b570ddcf-7f70-4502-b340-b1e4c02d204c",
+        "text": (
+            "All programs have to manage the way they use a computer’s memory"
+            " while running. Some languages have garbage collection that"
+            " constantly looks for no longer used memory as the program runs;"
+            " in other languages, the programmer must explicitly allocate and"
+            " free the memory. "
+        ),
+        "annotation": None,
+        "author": "Klabnik, Steve & Nichols, Carol",
+        "title": "The Rust Programming Language",
+    },
+    {
+        "id": "8b0e0173-6b3b-4bea-8361-175e259311df",
+        "text": "numeral.",
+        "annotation": "numeral",
+        "author": "Lameres, Brock J",
+        "title": "Introduction to Logic Circuits & Logic Design With Verilog",
+    },
+    {
+        "id": "9abdd7f9-2bf3-46d5-b075-2a1867d0b3e9",
+        "text": "nibble.",
+        "annotation": "nibble",
+        "author": "Lameres, Brock J",
+        "title": "Introduction to Logic Circuits & Logic Design With Verilog",
+    },
+    {
+        "id": "23547bce-8c37-44a6-a586-ae431b8f4def",
+        "text": (
+            "The leftmost bit in a binary number is called the Most Significant"
+            " Bit (MSB). The rightmost bit in a binary number is called the Least"
+            " Significant Bit (LSB)\n"
+        ),
+        "annotation": None,
+        "author": "Lameres, Brock J",
+        "title": "Introduction to Logic Circuits & Logic Design With Verilog",
+    },
+]
---
+++ tests/test_functions.py
@@ -0,0 +1,199 @@
+"""This module contains tests for the functionality in `functions.py`. It contains
+the following tests:
+
+* test_query_bookmarks_from_ereader: Tests the querying of bookmarks from the erader
+sqlite database.
+
+* test_add_bookmark_to_md_new: Tests adding a bookmark to a new markdown file.
+
+* test_add_bookmark_to_md_existing: Tests adding a bookmark to an existing markdown
+file.
+
+* test_query_bookmarks_from_markdown: Tests querying the bookmarks from markdown files.
+
+* test_query_bookmarks_from_markdown_ignore: Tests that markdown files without a proper
+name are ignored when querying the bookmarks.
+
+* test_setup_config_file_doesnt_exist: Tests the that a proper `Config` object is
+created when a config file exists.
+
+* test_setup_config_file_exist:Tests the that a proper `Config` object is created when a
+config file doesn't exist.
+"""
+from pathlib import Path
+from unittest.mock import patch, mock_open
+
+from .references import (
+    BOOKMARKS_TO_ADD,
+    REFERENCE_MARKDOWN,
+    BOOKMARKS_QUERIED_FROM_MD,
+    EXPECTED_BOOKMARKS_SQLITE,
+)
+from kobo_highlights.functions import (
+    setup_config,
+    query_bookmarks_from_ereader,
+    query_bookmarks_from_markdown,
+    add_bookmark_to_md,
+)
+from kobo_highlights.config import Config, ConfigError
+
+
+def test_query_bookmarks_from_ereader(tmp_path):
+    """Use a pre-defined sqlite file shipped with the tests to check that the query
+    functionality works as expected. The pytest temporary path `tmp_path` is used as
+    the local directory in which the database is copied.
+    """
+
+    TEST_ROOT: Path = Path(__file__).parent
+    # Fake ereader directory:
+    SQLITE_FILEPATH: Path = TEST_ROOT / "KoboReader.sqlite"
+
+    # Fake local dir for the copy of the database:
+    LOCAL_DIR: Path = tmp_path
+
+    assert not (LOCAL_DIR / "KoboReader.sqlite").is_file()
+
+    queried_bookmarks: list[dict] = query_bookmarks_from_ereader(
+        SQLITE_FILEPATH, LOCAL_DIR
+    )
+
+    assert queried_bookmarks == EXPECTED_BOOKMARKS_SQLITE
+    assert not (LOCAL_DIR / "KoboReader.sqlite").is_file()
+
+
+def test_add_bookmark_to_md_new(tmp_path):
+    """Test adding bookmarks to a markdown file that doesn't previously exist. This test
+    uses the first bookmark contained in`BOOKMARKS_TO_ADD`. The resulting markdown text
+    should be the first string from `REFERENCE_MARKDOWN`.
+
+    The function is called on a mocked file and then it is checked that the proper
+    write methods were called with the markdown content from `REFERENCE_MARKDOWN`.
+    """
+
+    # Name of the markdown file from the bookmark characteristics.
+    filename: str = (
+        f"{BOOKMARKS_TO_ADD[0]['title']} - {BOOKMARKS_TO_ADD[0]['author']}.md"
+    )
+    filepath: Path = tmp_path / filename
+
+    # When the markdown file doesn't previously exist, add_bookmark_to_md() uses the
+    # method Path.write_text().
+    with (
+        patch.object(Path, "write_text") as mock_write_text,
+        patch.object(Path, "is_file", return_value=False),
+    ):
+
+        assert not filepath.is_file()
+        add_bookmark_to_md(BOOKMARKS_TO_ADD[0], tmp_path)
+        mock_write_text.assert_called_once_with(REFERENCE_MARKDOWN[0])
+
+
+def test_add_bookmark_to_md_existing(tmp_path):
+    """Test adding bookmarks to a markdown file that already exists. This test uses the
+    second bookmark contained in`BOOKMARKS_TO_ADD`. The resulting markdown text
+    should be the second string from `REFERENCE_MARKDOWN`.
+
+    The function is called on a mocked file and then it is checked that the proper
+    write methods were called with the markdown content from `REFERENCE_MARKDOWN`.
+    """
+    filename: str = (
+        f"{BOOKMARKS_TO_ADD[1]['title']} - {BOOKMARKS_TO_ADD[1]['author']}.md"
+    )
+    filepath: Path = tmp_path / filename
+    # When the markdown file does already exist, add_bookmark_to_md() uses the method
+    # Path.open() to open the file and then the write method from the file object.
+    with (
+        patch.object(Path, "open", mock_open()) as mocked_path_open,
+        patch.object(Path, "is_file", return_value=True),
+    ):
+
+        assert filepath.is_file()
+        add_bookmark_to_md(BOOKMARKS_TO_ADD[1], tmp_path)
+
+        mocked_path_open.assert_called_once_with("a")
+        mocked_path_open().write.assert_called_once_with(REFERENCE_MARKDOWN[1])
+
+
+def test_query_bookmarks_from_markdown(tmp_path):
+    """Test that bookmarks are properly queried from markdown documents. To do so,
+    a markdown file is created under the temporal path `tmp_path` with the contents
+    from `REFERENCE_MARKDOWN`.
+
+    The function `query_bookmarks_from_markdown()` is then applied and the results are
+    compared with the reference `BOOKMARKS_QUERIED_FROM_MD`.
+    """
+
+    complete_markdown: str = REFERENCE_MARKDOWN[0] + REFERENCE_MARKDOWN[1]
+
+    title: str = BOOKMARKS_QUERIED_FROM_MD[0]["title"]
+    author: str = BOOKMARKS_QUERIED_FROM_MD[0]["author"]
+
+    # According to convention.
+    md_filename: str = f"{title} - {author}.md"
+    md_filepath: Path = tmp_path / md_filename
+    md_filepath.write_text(complete_markdown)
+
+    queried_bookmarks = query_bookmarks_from_markdown(tmp_path)
+    assert queried_bookmarks == BOOKMARKS_QUERIED_FROM_MD
+
+
+def test_query_bookmarks_from_markdown_ignore(tmp_path):
+    """Test that the function `query_bookmarks_from_markdown` ignores files without
+    the right filename. To do so three markdown files are created under the temporal
+    path `tmp_path` with slightly wrong names (according to the convention). The files
+    contain proper bookmarks (from REFERENCE_MARKDOWN), but because of the filename
+    they should be ignored by `query_bookmarks_from_markdown`.
+    """
+
+    complete_markdown: str = REFERENCE_MARKDOWN[0] + REFERENCE_MARKDOWN[1]
+    md_filenames: list[str] = [
+        "title- author.md",
+        "title -author.md",
+        "title-author.md",
+    ]
+
+    for filename in md_filenames:
+        md_filepath: Path = tmp_path / filename
+        md_filepath.write_text(complete_markdown)
+
+    queried_bookmarks = query_bookmarks_from_markdown(tmp_path)
+    assert queried_bookmarks == []
+
+
+def test_setup_config_file_doesnt_exist(tmp_path):
+    """Test that the function `setup_config()` produces a proper config object when
+    the config file doesn't exists. Notably, this test doesn't test the inner logic from
+    the `Config` class, that is done in `test_config.py`, here the entire functionality
+    of `Config` is mocked.
+    """
+
+    ereader_dir: Path = tmp_path / "ereader"
+    target_dir: Path = tmp_path / "target"
+    config_filpath: Path = tmp_path / "config.toml"
+
+    return_config = Config(ereader_dir=ereader_dir, target_dir=target_dir)
+
+    with (
+        patch.object(Config, "from_file") as mock_config_from_file,
+        patch.object(Config, "create_interactively", return_value=return_config),
+    ):
+
+        mock_config_from_file.side_effect = ConfigError("No config file")
+        assert setup_config(config_filpath) == return_config
+
+
+def test_setup_config_file_exist(tmp_path):
+    """Test that the function `setup_config()` produces a proper config object when
+    the config file exists. Notably, this test doesn't test the inner logic from the
+    `Config` class, that is done in `test_config.py`, here the entire functionality
+    of `Config` is mocked.
+    """
+
+    ereader_dir: Path = tmp_path / "ereader"
+    target_dir: Path = tmp_path / "target"
+    config_filpath: Path = tmp_path / "config.toml"
+
+    return_config = Config(ereader_dir=ereader_dir, target_dir=target_dir)
+
+    with patch.object(Config, "from_file", return_value=return_config):
+        assert setup_config(config_filpath) == return_config
